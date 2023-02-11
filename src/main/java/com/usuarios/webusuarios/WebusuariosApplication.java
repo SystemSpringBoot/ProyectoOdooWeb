0b4cf470b4c9a6e4eb8651606c073af733db77cf
dario4732 <torres.dario@oca.com.ar>
2023-02-11 10:49:10 -0300

    “Creado”

---
+++ src/main/java/com/usuarios/webusuarios/WebusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.usuarios.webusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebusuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usuarios/webusuarios/WebusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuarios.webusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
